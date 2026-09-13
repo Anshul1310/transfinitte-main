3907c6aab1d7472e6bd1063b3ba978a6dc168286
S@twik <satwiksinha1011@gmail.com>
2026-09-14 02:08:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -126,10 +126,6 @@ const FooterMobile = memo(() => (
               text="Locate Us"
             />
             <FooterLink
-              href="https://drive.google.com/file/d/1kZ9JWKb0OFPmNv4avGa1OEcq8W6-FxjJ/view"
-              text="Archive"
-            />
-            <FooterLink
               href={GOOGLE_CALENDAR_URL}
               text="Add to Calendar"
             />
@@ -186,10 +182,7 @@ const FooterDesktop = memo(() => (
             href="https://maps.app.goo.gl/oqqeVqML8Fb2Pdr57"
             text="Locate Us"
           />
-          <FooterLink
-            href="https://drive.google.com/file/d/1jjjwgRhEHz8pLegoh0GjU5KTq7Ni5zAs/view"
-            text="Archive"
-          />
+   
           <FooterLink
             href={GOOGLE_CALENDAR_URL}
             text="Add to Calendar"
