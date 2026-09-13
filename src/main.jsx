28df9ef36bb6e338bdfd1fd4c30efe24450a8303
S@twik <satwiksinha1011@gmail.com>
2026-09-14 01:25:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from './components/ThemeProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
