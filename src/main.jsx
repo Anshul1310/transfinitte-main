da58dac5a966f458c9faef14f4f28a9959c25d0f
S@twik <satwiksinha1011@gmail.com>
2026-09-14 01:45:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
-
+import { ThemeProvider } from "./components/ThemeProvider"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="dark">
       <App />
       <Toaster />
+      </ThemeProvider>
   </StrictMode>
 )
