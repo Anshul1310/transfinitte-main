b093a3fab30192e62872807e64f3504207be6480
S@twik <satwiksinha1011@gmail.com>
2026-09-14 01:32:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark">
       <App />
       <Toaster />
-    </ThemeProvider>
   </StrictMode>
 )
