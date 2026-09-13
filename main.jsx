328b45f601b19a4d0ee8350aa5abab69975f39b2
S@twik <satwiksinha1011@gmail.com>
2026-09-14 01:31:02 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -5,15 +5,15 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Toaster } from "@/components/ui/sonner"
 
-import { ThemeProvider } from './src/components/ThemeProvider'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark">
+    
       <Router>
         <App />
         <Toaster />
       </Router>
-    </ThemeProvider>
+   
   </React.StrictMode>,
 )
